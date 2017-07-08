247224c55d649cb08976256ea8a3bea041ff6e71
FyiurAmron <spamove@gmail.com>
2017-07-08 16:51:17 +0200

    objectification

--- js/tower.js
+++ js/tower.js
@@ -1,5 +1,24 @@
 'use strict';
 
+// helpers
+
+function readFile( filename, callback ) {
+    var req = new XMLHttpRequest();
+    req.open( "GET", filename, true );
+    req.onreadystatechange = function( evt ) {
+        if ( req.readyState !== 4 ) {
+            return;
+        }
+        if ( req.responseText !== null ) {
+            callback( req.responseText );
+        } else {
+            alert( "'" + filename + "' could not be loaded from server!" );
+            alert( "req.readyState === " + req.readyState + " req.status === " + req.status );
+        }
+    }
+    req.send( null );
+}
+
 // business logic
 
 const TILE_SIZE_X = 32;
@@ -12,13 +31,13 @@ const BOARD_SIZE_X = 16;
 const BOARD_SIZE_Y = 24;
 const BOARD_SIZE_TOTAL = BOARD_SIZE_X * BOARD_SIZE_Y;
 
-const board = {
+const _board = {
     background: new Array( BOARD_SIZE_TOTAL ),
     tiles: new Array( BOARD_SIZE_TOTAL ),
     content: new Array( BOARD_SIZE_TOTAL )
 };
 
-var hero = {
+const _hero = {
     x: 0, y: 0, z: 0,
     hp: 100, att: 10, def: 10,
     xp: 0, level: 0, gold: 0,
@@ -38,84 +57,81 @@ const id = {
     inventoryPanel: "inventoryPanel"
 }
 
-const dom = {};
+const _dom = {};
 
-function readFile( filename, callback ) {
-    var req = new XMLHttpRequest();
-    req.open( "GET", filename, true );
-    req.onreadystatechange = function( evt ) {
-        if ( req.readyState !== 4 ) {
-            return;
-        }
-        if ( req.responseText !== null ) {
-            callback( req.responseText );
-        } else {
-            alert( "'" + filename + "' could not be loaded from server!" );
-            alert( "req.readyState === " + req.readyState + " req.status === " + req.status );
-        }
-    }
-    req.send( null );
+const game = {
+    hero: _hero,
+    board: _board,
+    dom: _dom
 }
 
-function createDefaultBackground( boardBackgroundDom ) {
+game.createDefaultBackground = function() {
+    var bb = this.board.background;
+    var dbb = this.dom.boardBackground;
     for ( var i = 0, x = 0/*, y = 0*/; i < BOARD_SIZE_TOTAL; i++, x++ ) {
         var tile = document.createElement( DIV_TAG );
         tile.classList.add( TILE_CLASS );
-        board.background[i] = tile;
+        bb[i] = tile;
         if ( x === BOARD_SIZE_X ) {
-            dom.boardBackground.appendChild( document.createElement( BR_TAG ) );
+            dbb.appendChild( document.createElement( BR_TAG ) );
             x = 0;
             //y++;
         }
-        dom.boardBackground.appendChild( tile ); // use positioning instead?
-        setBackground( i, DEFAULT_BOARD_CONTENT );
+        dbb.appendChild( tile ); // use positioning instead?
+        this.setBackground( i, DEFAULT_BOARD_CONTENT );
     }
 }
 
-function createEmptyBoard( boardDom ) {
+game.createEmptyBoard = function() {
+    var bt = this.board.tiles;
+    var db = this.dom.board;
     for ( var i = 0, x = 0/*, y = 0*/; i < BOARD_SIZE_TOTAL; i++, x++ ) {
         var tile = document.createElement( DIV_TAG );
         tile.classList.add( TILE_CLASS );
-        board.tiles[i] = tile;
+        bt[i] = tile;
         if ( x === BOARD_SIZE_X ) {
-            dom.board.appendChild( document.createElement( BR_TAG ) );
+            db.appendChild( document.createElement( BR_TAG ) );
             x = 0;
             //y++;
         }
-        dom.board.appendChild( tile ); // use positioning instead?
-        setTile( i, null );
+        db.appendChild( tile ); // use positioning instead?
+        this.setTile( i, null );
     }
 }
 
-function setBackground( x, y, content ) {
+game.createInventory = function() {
+
+}
+
+game.setBackgroundXY = function( x, y, content ) {
     setBackground( x + y * BOARD_SIZE_X, content );
 }
 
-function setTile( x, y, content ) {
+game.setTileXY = function( x, y, content ) {
     setTile( x + y * BOARD_SIZE_X, content );
 }
 
-function setBackground( index, contentNr ) {
+game.setBackground = function( index, contentNr ) {
     var tilesetX = ( ( contentNr % TILESET_WIDTH ) | 0 ) * TILE_SIZE_X;
     var tilesetY = ( ( contentNr / TILESET_WIDTH ) | 0 ) * TILE_SIZE_Y;
-    board.background[index].style = "background-position: -" + tilesetX + "px -" + tilesetY + "px;";
+    this.board.background[index].style = "background-position: -" + tilesetX + "px -" + tilesetY + "px;";
 }
 
-function setTile( index, contentNr ) {
+game.setTile = function( index, contentNr ) {
     if ( contentNr == null ) {
-        board.tiles[index].style = "opacity: 0;";
-        board.content[index] = contentNr;
+        this.board.tiles[index].style = "opacity: 0;";
+        this.board.content[index] = contentNr;
         return;
     }
-    board.content[index] = contentNr;
+    this.board.content[index] = contentNr;
     var tilesetX = ( ( contentNr % TILESET_WIDTH ) | 0 ) * TILE_SIZE_X;
     var tilesetY = ( ( contentNr / TILESET_WIDTH ) | 0 ) * TILE_SIZE_Y;
-    board.tiles[index].style = "background-position: -" + tilesetX + "px -" + tilesetY + "px;";
+    this.board.tiles[index].style = "background-position: -" + tilesetX + "px -" + tilesetY + "px;";
 
 if ( contentNr != 22 ) {
 return;
 }
-board.tiles[index].animate([
+this.board.tiles[index].animate([
   // keyframes
   { transform: 'rotate(0deg)' }, 
   { transform: 'rotate(360deg)' }
@@ -127,7 +143,7 @@ board.tiles[index].animate([
 
 }
 
-var mapCharMapping = [
+game.mapCharMapping = [
     ['+',0],
     ['&',4],
     ['#',7],
@@ -138,47 +154,52 @@ var mapCharMapping = [
     ['|',22],
 ];
 
-var mapCharMap = new Map( mapCharMapping );
+game.mapCharMap = new Map( game.mapCharMapping );
 
-function updateTitle() {
+game.updateTitle = function() {
+    var hero = this.hero;
     document.title = "X: " + hero.x + "\u2007 Y: " + hero.y + "\u2007 Z: " + hero.z;
 }
 
-function updateStats() {
-    dom.statPanel.innerHTML = "HP: " + hero.hp + " ATT: " + hero.att + " DEF: " + hero.def
+game.updateStats = function() {
+    var hero = this.hero;
+    this.dom.statPanel.innerHTML = "HP: " + hero.hp + " ATT: " + hero.att + " DEF: " + hero.def
                                + "<br />XP: " + hero.xp + " LEV: " + hero.level + " $$: " + hero.gold;
 }
 
-function updateInventory() {
+game.updateInventory = function() {
 
 }
 
-function updateBoard() {
+game.updateBoard = function() {
 
 }
 
-function update() {
-    updateTitle();
-    updateStats();
-    updateInventory();
-    updateBoard();
+game.update = function() {
+    this.updateTitle();
+    this.updateStats();
+    this.updateInventory();
+    this.updateBoard();
 }
 
-//document.addEventListener('DOMContentLoaded', function() {
-window.onload = function() {
+game.init = function() {
+    console.log( "game.init();" );
+    var dom = this.dom;
     dom.inventoryPanel = document.getElementById( id.inventoryPanel );
     dom.statPanel = document.getElementById( id.statPanel );
     dom.boardBackground = document.getElementById( id.boardBackground );
     dom.board = document.getElementById( id.board );
 
-    createDefaultBackground( dom.boardBackground );
-    createEmptyBoard( dom.board );
+    game.createDefaultBackground();
+    game.createEmptyBoard();
 
     var widthStr = "width: " + TILE_SIZE_X * BOARD_SIZE_X + "px; ";
     dom.statPanel.style = widthStr;
     dom.inventoryPanel.style = widthStr;
     dom.boardBackground.style = widthStr;
     dom.board.style = widthStr + "top: -" + TILE_SIZE_Y * BOARD_SIZE_Y + "px; ";
+
+    var thisGame = this;
 
     readFile( "data/map0.txt", function( mapStr ) {
         for ( var i = 0, pos = -1, len = mapStr.length; i < len; i++ ) {
@@ -190,15 +211,21 @@ window.onload = function() {
             if ( c === '.' ) {
                 continue;
             }
-            var contentNr = DEFAULT_BOARD_CONTENT;
-            //alert( pos + " " + c );
-            
-            setTile( pos, mapCharMap.get( c ) );
+
+            var contentNr = thisGame.mapCharMap.get( c );
+            if ( contentNr == null ) {
+                console.log( "unknown map char '" + c + "' [0x" + c.charCodeAt( 0 ).toString( 16 ) + "]" );
+            }
+           
+            thisGame.setTile( pos, contentNr );
         }
     } );
 
-    updateTitle();
-    updateStats();
+    this.updateTitle();
+    this.updateStats();
 }
-;
-//);
+
+//document.addEventListener('DOMContentLoaded', game.init );
+window.onload = function() {
+    game.init();
+};
