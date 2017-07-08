97761548662dd3ed412d722b21384ccbbd36ee77
FyiurAmron <spamove@gmail.com>
2017-07-09 01:17:37 +0200

    heavy refactoring & enhancements

---
+++ js/junk.js
@@ -0,0 +1,11 @@
+/*
+this.bg.tiles[index].animate([
+  // keyframes
+  { transform: 'rotate(0deg)' }, 
+  { transform: 'rotate(360deg)' }
+], { 
+  // timing options
+  duration: 1000,
+  iterations: Infinity
+});
+*/
--- js/tower.js
+++ js/tower.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/* jslint bitwise: true */
+/* jslint node: true */
+/* jslint laxbreak: true */
+
 // helpers
 
 function readFile( filename, callback ) {
@@ -25,184 +29,131 @@ const TILE_SIZE_X = 32;
 const TILE_SIZE_Y = 32;
 const TILESET_WIDTH = 11;
 
-const DEFAULT_BOARD_CONTENT = 5;
+const DEFAULT_BACKGROUND_TYPE = 5;
 
 const BOARD_SIZE_X = 16;
 const BOARD_SIZE_Y = 24;
 const BOARD_SIZE_TOTAL = BOARD_SIZE_X * BOARD_SIZE_Y;
 
-const _board = {
-    background: new Array( BOARD_SIZE_TOTAL ),
-    tiles: new Array( BOARD_SIZE_TOTAL ),
-    content: new Array( BOARD_SIZE_TOTAL ),
-};
+class Board {
+  constructor( size, nameId ) {
+    this.dom = null;
+    this.tiles = new Array( size );
+    this.content = new Array( size );
+    this.tileClass = TILE_CLASS + "-" + nameId;
+    this.rowIdPrefix = BOARD_ROW_CLASS + "-" + nameId + "-";
+  }
+
+  init( dom ) {
+    this.dom = dom;
+    var t = this.tiles;
+    var row = document.createElement( DIV_TAG );
+    row.classList.add( BOARD_ROW_CLASS );
+    row.id = this.rowIdPrefix + "0";
+    dom.appendChild( row );
+    for ( var i = 0, x = 0, y = 0; i < BOARD_SIZE_TOTAL; i++, x++ ) {
+        var tile = document.createElement( DIV_TAG );
+        tile.classList.add( TILE_CLASS );
+        t[i] = tile;
+        if ( x === BOARD_SIZE_X ) {
+            row = document.createElement( DIV_TAG );
+            row.classList.add( BOARD_ROW_CLASS );
+            y++;
+            row.id = this.rowIdPrefix + y;
+            dom.appendChild( row );
+            x = 0;
+        }
+        tile.style.left = x * TILE_SIZE_X + "px";
+        tile.style.top = y * TILE_SIZE_Y + "px";
+        row.appendChild( tile );
+        //this.setElement( i, undefined );
+    }
+  }
+
+  setElement( elemIdx, typeId ) {
+    var tileDom = this.tiles[elemIdx];
+
+    this.content[elemIdx] = typeId;
+
+    tileDom.className = "";
+    var cl = tileDom.classList;
+    cl.add( TILE_CLASS );
+    cl.add( this.tileClass );
+    cl.add( TILE_TYPE_CLASS_PREFIX + typeId );
+
+    if ( typeId === undefined ) {
+        return;
+    }
+
+    var tilesetX = ( ( typeId % TILESET_WIDTH ) | 0 ) * TILE_SIZE_X;
+    var tilesetY = ( ( typeId / TILESET_WIDTH ) | 0 ) * TILE_SIZE_Y;
+    tileDom.style.backgroundPosition = "-" + tilesetX + "px -" + tilesetY + "px";
+  }
+}
 
 const INVENTORY_SIZE = BOARD_SIZE_X;
-
-const _hero = {
-    x: 0, y: 0, z: 0,
-    hp: 100, att: 10, def: 10,
-    xp: 0, level: 0, gold: 0,
-    inv: new Array( INVENTORY_SIZE ),
-};
 
 // DOM-related
 
 const DIV_TAG = "div";
 
 const TILE_CLASS = "tile";
-const TILE_BACKGROUND_CLASS = TILE_CLASS + "-bg";
-const TILE_FOREGROUND_CLASS = TILE_CLASS + "-fg";
-const TYPE_SUFFIX = "-type-";
-const TILE_BACKGROUND_TYPE_CLASS_PREFIX = TILE_BACKGROUND_CLASS + TYPE_SUFFIX;
-const TILE_FOREGROUND_TYPE_CLASS_PREFIX = TILE_FOREGROUND_CLASS + TYPE_SUFFIX;
-const TILE_EMPTY_CLASS_SUFFIX = "empty";
-
+/* const TYPE_SUFFIX = "-type-"; */
 const BOARD_ROW_CLASS = "board-row";
-const BOARD_ROW_ID_PREFIX = BOARD_ROW_CLASS + "-";
-const BOARD_BACKGROUND_ROW_ID_PREFIX = BOARD_ROW_CLASS + "-bg-";
+/* const TILE_EMPTY_CLASS_SUFFIX = "empty"; */
+const TILE_TYPE_CLASS_PREFIX = TILE_CLASS + "-type-";
 
 const id = {
-    board: "board",
     boardBackground: "boardBackground",
+    boardForeground: "boardForeground",
     statPanel: "statPanel",
     inventoryPanel: "inventoryPanel",
     gamePanel: "gamePanel",
 };
 
-const mapCharMapping = [
-    ['+', 0],
-    ['&', 4],
-    ['#', 7],
-    ['>', 10],
-    ['<', 11],
-    ['~', 12],
-    ['^', 13],
-    ['F', 22],
-    ['!', 31],
-    ['|', 32],
+const mapBgCharRaw = [
+    [ '#', 7 ],
+    [ '~', 12 ],
+    [ '^', 13 ],
+    [ 'B', 13 ],
+];
+
+const mapFgCharRaw = [
+    [ '+', 0 ],
+    [ '&', 4 ],
+    [ '>', 10 ],
+    [ '<', 11 ],
+    [ 'B', 22 ],
+    [ '!', 31 ],
+    [ '|', 32 ],
+    [ 'z', 76 ],
+    [ 'Z', 78 ],
 ];
 
 class Game {
   constructor() {
-    this.hero = _hero;
-    this.board = _board;
+    this.hero = {
+      x: 0, y: 0, z: 0,
+      hp: 100, att: 10, def: 10,
+      xp: 0, level: 0, gold: 0,
+      inv: new Array( INVENTORY_SIZE ),
+    };
+    this.bg = new Board( BOARD_SIZE_TOTAL, "bg" );
+    this.fg = new Board( BOARD_SIZE_TOTAL, "fg" );
     this.dom = {};
-    this.mapCharMap = new Map( mapCharMapping );
+    this.mapBgCharMap = new Map( mapBgCharRaw );
+    this.mapFgCharMap = new Map( mapFgCharRaw );
   }
 
-  createDefaultBackground() {
-    var bb = this.board.background;
-    var dbb = this.dom.boardBackground;
-    var row = document.createElement( DIV_TAG );
-    row.classList.add( BOARD_ROW_CLASS );
-    row.id = BOARD_BACKGROUND_ROW_ID_PREFIX + "0";
-    dbb.appendChild( row );
-    for ( var i = 0, x = 0, y = 0; i < BOARD_SIZE_TOTAL; i++, x++ ) {
-        var tile = document.createElement( DIV_TAG );
-        tile.classList.add( TILE_CLASS );
-        bb[i] = tile;
-        if ( x === BOARD_SIZE_X ) {
-            row = document.createElement( DIV_TAG );
-            row.classList.add( BOARD_ROW_CLASS );
-            row.id = BOARD_BACKGROUND_ROW_ID_PREFIX + y;
-            dbb.appendChild( row );
-            x = 0;
-            y++;
-        }
-        row.appendChild( tile ); // use positioning instead?
-        this.setBackground( i, DEFAULT_BOARD_CONTENT );
-    }
+  setFgXY( x, y, typeId ) {
+    this.bg.setElement( x + y * BOARD_SIZE_X, typeId );
   }
 
-  createEmptyBoard() {
-    var bt = this.board.tiles;
-    var db = this.dom.board;
-    var row = document.createElement( DIV_TAG );
-    row.classList.add( BOARD_ROW_CLASS );
-    row.id = BOARD_ROW_ID_PREFIX + "0";
-    db.appendChild( row );
-    for ( var i = 0, x = 0, y = 0; i < BOARD_SIZE_TOTAL; i++, x++ ) {
-        var tile = document.createElement( DIV_TAG );
-        tile.classList.add( TILE_CLASS );
-        bt[i] = tile;
-        if ( x === BOARD_SIZE_X ) {
-            row = document.createElement( DIV_TAG );
-            row.classList.add( BOARD_ROW_CLASS );
-            row.id = BOARD_ROW_ID_PREFIX + y;
-            db.appendChild( row );
-            x = 0;
-            y++;
-        }
-        row.appendChild( tile ); // use positioning instead?
-        this.setTile( i, null );
-    }
+  setBgXY( x, y, typeId ) {
+    this.fg.setElement( x + y * BOARD_SIZE_X, typeId );
   }
 
   createInventory() {
-
-  }
-
-  setBackgroundXY( x, y, content ) {
-    setBackground( x + y * BOARD_SIZE_X, content );
-  }
-
-  setTileXY( x, y, content ) {
-    setTile( x + y * BOARD_SIZE_X, content );
-  }
-
-  setBackground( index, typeNr ) {
-    var tileDom = this.board.background[index];
-    var tilesetX = ( ( typeNr % TILESET_WIDTH ) | 0 ) * TILE_SIZE_X;
-    var tilesetY = ( ( typeNr / TILESET_WIDTH ) | 0 ) * TILE_SIZE_Y;
-    tileDom.style = "background-position: -" + tilesetX + "px -" + tilesetY + "px;";
-    tileDom.className = "";
-    var cl = tileDom.classList;
-    cl.add( TILE_CLASS );
-    cl.add( TILE_BACKGROUND_CLASS );
-    cl.add( TILE_BACKGROUND_TYPE_CLASS_PREFIX + typeNr );
-}
-
-  setTile( index, typeNr ) {
-    var tileDom = this.board.tiles[index];
-    if ( typeNr == null ) {
-        this.board.content[index] = typeNr;
-
-        tileDom.style = "";
-        tileDom.className = "";
-        var cl = tileDom.classList;
-        cl.add( TILE_CLASS );
-        cl.add( TILE_FOREGROUND_CLASS );
-        cl.add( TILE_FOREGROUND_TYPE_CLASS_PREFIX + TILE_EMPTY_CLASS_SUFFIX );
-        return;
-    }
-    this.board.content[index] = typeNr;
-
-    var tilesetX = ( ( typeNr % TILESET_WIDTH ) | 0 ) * TILE_SIZE_X;
-    var tilesetY = ( ( typeNr / TILESET_WIDTH ) | 0 ) * TILE_SIZE_Y;
-
-    tileDom.style = "background-position: -" + tilesetX + "px -" + tilesetY + "px;";
-    tileDom.className = "";
-    var cl = tileDom.classList;
-    cl.add( TILE_CLASS );
-    cl.add( TILE_FOREGROUND_CLASS );
-    cl.add( TILE_FOREGROUND_TYPE_CLASS_PREFIX + typeNr );
-
-// TODO add proper CSS animation below
-/*
-if ( typeNr != 22 ) {
-return;
-}
-this.board.tiles[index].animate([
-  // keyframes
-  { transform: 'rotate(0deg)' }, 
-  { transform: 'rotate(360deg)' }
-], { 
-  // timing options
-  duration: 1000,
-  iterations: Infinity
-});
-*/
 
   }
 
@@ -214,7 +165,7 @@ this.board.tiles[index].animate([
   updateStats() {
     var hero = this.hero;
     this.dom.statPanel.innerHTML = "HP: " + hero.hp + " ATT: " + hero.att + " DEF: " + hero.def
-                               + "<br />XP: " + hero.xp + " LEV: " + hero.level + " $$: " + hero.gold;
+                           + "<br />XP: " + hero.xp + " LEV: " + hero.level + " $$: " + hero.gold;
   }
 
   updateInventory() {
@@ -230,7 +181,37 @@ this.board.tiles[index].animate([
     this.updateStats();
     this.updateInventory();
     this.updateBoard();
-}
+  }
+
+  loadMap( mapNr ) {
+    var bg = this.bg;
+    var fg = this.fg;
+    var bgIdMap = this.mapBgCharMap;
+    var fgIdMap = this.mapFgCharMap;
+
+    readFile( "data/map" + mapNr + ".txt", function( mapStr ) {
+        for ( var i = 0, pos = -1, len = mapStr.length; i < len; i++ ) {
+            var c = mapStr.charAt( i );
+            if ( c === '\n' || c === '\r' ) {
+                continue;            
+            }
+            pos++;
+
+            var typeIdBg = bgIdMap.get( c );
+            var typeIdFg = fgIdMap.get( c );
+            
+            if ( typeIdBg === undefined ) {
+                if ( typeIdFg === undefined ) {
+                    console.log( "unknown map char '" + c + "' [0x" + c.charCodeAt( 0 ).toString( 16 ) + "]" );
+                }
+                typeIdBg = DEFAULT_BACKGROUND_TYPE;
+            }
+           
+            bg.setElement( pos, typeIdBg );
+            fg.setElement( pos, typeIdFg );
+        }
+    } );
+  }
 
   init() {
     console.log( "game.init();" );
@@ -238,11 +219,11 @@ this.board.tiles[index].animate([
     dom.inventoryPanel = document.getElementById( id.inventoryPanel );
     dom.statPanel = document.getElementById( id.statPanel );
     dom.boardBackground = document.getElementById( id.boardBackground );
-    dom.board = document.getElementById( id.board );
+    dom.boardForeground = document.getElementById( id.boardForeground );
     dom.gamePanel = document.getElementById( id.gamePanel );
 
-    game.createDefaultBackground();
-    game.createEmptyBoard();
+    this.bg.init( dom.boardBackground );
+    this.fg.init( dom.boardForeground );
 
     var widthStr = "width: " + TILE_SIZE_X * BOARD_SIZE_X + "px; ";
     dom.statPanel.style = widthStr;
@@ -254,28 +235,7 @@ this.board.tiles[index].animate([
     dom.statPanel.innerHTML = "";
     dom.inventoryPanel.innerHTML = "";
 
-    var thisGame = this;
-
-    readFile( "data/map0.txt", function( mapStr ) {
-        for ( var i = 0, pos = -1, len = mapStr.length; i < len; i++ ) {
-            var c = mapStr.charAt( i );
-            if ( c === '\n' || c === '\r' ) {
-                continue;            
-            }
-            pos++;
-            if ( c === '.' ) {
-                continue;
-            }
-
-            var contentNr = thisGame.mapCharMap.get( c );
-            if ( contentNr == null ) {
-                console.log( "unknown map char '" + c + "' [0x" + c.charCodeAt( 0 ).toString( 16 ) + "]" );
-            }
-           
-            thisGame.setTile( pos, contentNr );
-        }
-    } );
-
+    this.loadMap( "0" );
     this.update();
   }
 }
@@ -284,6 +244,6 @@ var game;
 
 //document.addEventListener('DOMContentLoaded', game.init );
 window.onload = function() {
-    game = new Game()
+    game = new Game();
     game.init();
 };
